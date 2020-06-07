c55e87a3b68db9ea678fa0f3927605aefad735ac
Nastya <realovka@mail.ru>
2020-06-07 09:46:04 +0300

    4 commit

--- src/com/company/Division.java
+++ src/com/company/Division.java
@@ -3,6 +3,6 @@ package com.company;
 public class Division implements Operation {
     @Override
     public double calculate(int a, int b) {
-        return (double) a/(double) b;
+        return (double) a/ b;
     }
 }
